d26cb477da2081c103157b8fcb186d7a77e3d8aa
JaparmySholly <japarmySholly@gmail.com>
2023-01-02 16:58:29 +0000

    Test driven..

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''A unittest module for the max_integer function.
+'''
+import unittest
+from importlib import import_module
+
+
+class TestMaxInteger(unittest.TestCase):
+    '''Tests the max_integer function.
+    '''
+    def test_area(self):
+        '''The equality tests for the max_integer function.
+        '''
+        max_integer = import_module('6-max_integer', '..').max_integer
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([7, 2, -3, 45, 6]), 45)
+        self.assertEqual(max_integer([7, 2, 45, 6]), 45)
+        self.assertEqual(max_integer([-7, -2, -45, -6]), -2)
+        self.assertEqual(max_integer([-7, 1, -18, -6]), 1)
